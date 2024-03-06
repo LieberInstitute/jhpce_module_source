07f6f196a2b97af33b429cc919a8f5744edf38dd
Nick-Eagles <nick.eagles@libd.org>
2024-03-06 16:11:25 -0500

    Add a script for ensuring permissions are correct on all modules

---
+++ fix_permissions.sh
@@ -0,0 +1,25 @@
+#   Ensure permissions are correct on all source and lua files:
+#   'ordinary_permissions' controls permissions on all lua files and non-conda-
+#   environment source files. 'conda_permissions' controls permissions on conda
+#   environments.
+
+ordinary_permissions=775
+conda_permissions=555
+src_dir=/jhpce/shared/libd/core
+lua_dir=/jhpce/shared/libd/modulefiles
+
+echo "Setting default permissions on all source files..."
+chmod -R $ordinary_permissions $src_dir
+
+echo "Setting more-restrictive permissions on conda/ virtual environments..."
+for env_dir in $(ls -1d ${src_dir}/*/*/*_env); do
+    echo "[Conda/venv dir] $env_dir"
+    chmod -R $conda_permissions $env_dir
+done
+
+echo "Setting open permissions on 'r_nac'..."
+chmod 777 -R ${src_dir}/r_nac/1.0/nac_env
+
+echo "Setting default permisions on all lua files..."
+chmod -R $ordinary_permissions $lua_dir
+echo "Done."
